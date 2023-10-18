b56accd49d9a91b1c88edde14a71e0e54ea7f7c1
Nate Pierce <nate.pierce@maymobility.com>
2023-10-18 13:53:36 -0400

    move repo for xampp

---
+++ submit_form.php
@@ -0,0 +1,29 @@
+<?php
+// get the data from the form
+$umid = $_POST['umid'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$project_name = $_POST['project_name'];
+$email_address = $_POST['email_address'];
+$phone_number = $_POST['phone_number'];
+$seat = $_POST['seat'];
+
+// do some calculations
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Submitted</title>
+    <link rel="stylesheet" type="text/css" href="styles/index.css">
+</head>
+
+<body>
+    <main>
+        <h1>Hell yeah</h1>
+    </main>
+</body>
+
+</html>
